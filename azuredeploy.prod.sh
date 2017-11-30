11fe5923e4ff51d471171850dc82438fd8fbf3e1
Nick Ward <nickward@microsoft.com>
2017-11-30 21:02:22 +0000

    update cr lf

--- azuredeploy.prod.sh
+++ azuredeploy.prod.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-
 az login
-
 az group create --name hectagon-aks --location "centralus"
 #az group deployment create --resource-group hectagon-aks --template-file azuredeploy.json --parameters @azuredeploy.parameters.prod.json
 az aks create --resource-group hectagon-aks --name hectagonk8s2 --agent-count 1 --generate-ssh-keys
-
 az aks get-credentials --resource-group=hectagon-aks --name=hectagonk8s2
-
 kubectl create -f aspnetcoreapp.yml
-
 kubectl get service aspnetcoreapp --watch
+
